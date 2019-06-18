eb7ffb7aa803605082ca3adf145caf79c798d87a
Justin Field <fieldju@gmail.com>
2019-06-18 14:57:55 -0700

    feat: make server port configurable

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,7 +6,7 @@ import path from 'path';
 import { publicRoot } from './const/appPaths';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.redirect(301, '/dashboard'));
 
