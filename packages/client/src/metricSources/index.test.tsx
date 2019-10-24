beb08db5a5a6b5a37846d4909bf000b6464f50fb
Melana Hammel <Melana.Hammel@nike.com>
2019-10-24 16:06:49 -0700

    chore: refactored default graph mapper and added unit tests

---
+++ packages/client/src/metricSources/index.test.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { defaultGraphDataMapper } from './index';
+
+it('generates default graph labels with populated parameters', () => {
+  const lifetime = 45;
+  const dataPointCount = 10;
+  const startTimeMillis = 1565215800000;
+  const stepMillis = 10000;
+
+  const expectedControlLabels: number[] = [
+    1565215800000,
+    1565216070000,
+    1565216340000,
+    1565216610000,
+    1565216880000,
+    1565217150000,
+    1565217420000,
+    1565217690000,
+    1565217960000,
+    1565218230000
+  ];
+
+  const { controlTimeLabels } = defaultGraphDataMapper(lifetime, dataPointCount, startTimeMillis, stepMillis);
+  expect(controlTimeLabels).toEqual(expectedControlLabels);
+});
+
+it('generates default graph labels with empty lifetime', () => {
+  const lifetime = 0;
+  const dataPointCount = 10;
+  const startTimeMillis = 1565215800000;
+  const stepMillis = 10000;
+
+  const expectedControlLabels: number[] = [
+    1565215800000,
+    1565215810000,
+    1565215820000,
+    1565215830000,
+    1565215840000,
+    1565215850000,
+    1565215860000,
+    1565215870000,
+    1565215880000,
+    1565215890000
+  ];
+
+  const { controlTimeLabels } = defaultGraphDataMapper(lifetime, dataPointCount, startTimeMillis, stepMillis);
+  expect(controlTimeLabels).toEqual(expectedControlLabels);
+});
+
+it('generates empty graph labels with zero data points', () => {
+  const lifetime = 45;
+  const dataPointCount = 0;
+  const startTimeMillis = 1565215800000;
+  const stepMillis = 10000;
+
+  const expectedControlLabels: number[] = [];
+  const { controlTimeLabels } = defaultGraphDataMapper(lifetime, dataPointCount, startTimeMillis, stepMillis);
+  expect(controlTimeLabels).toEqual(expectedControlLabels);
+});
+
+it('generates default graph labels with parameters of zero', () => {
+  const lifetime = 0;
+  const dataPoints = 0;
+  const startTimeMillis = 0;
+  const stepMillis = 0;
+
+  const expectedControlLabels: number[] = [];
+  const { controlTimeLabels } = defaultGraphDataMapper(lifetime, dataPoints, startTimeMillis, stepMillis);
+  expect(controlTimeLabels).toEqual(expectedControlLabels);
+});
+
+it('generates default graph labels with empty parameters', () => {
+  const lifetime = NaN;
+  const dataPoints = NaN;
+  const startTimeMillis = NaN;
+  const stepMillis = NaN;
+
+  const expectedControlLabels: number[] = [];
+  const { controlTimeLabels } = defaultGraphDataMapper(lifetime, dataPoints, startTimeMillis, stepMillis);
+  expect(controlTimeLabels).toEqual(expectedControlLabels);
+});
--- packages/client/src/metricSources/index.tsx
+++ packages/client/src/metricSources/index.tsx
@@ -1,8 +1,9 @@
-import { CanaryMetricSetQueryConfig } from '../domain/Kayenta';
+import { CanaryMetricSetQueryConfig, MetricSetPairAttributes } from '../domain/Kayenta';
 import { MetricSourceIntegration } from './MetricSourceIntegration';
 import NewRelic from './NewRelic';
 import SignalFx from './SignalFx';
 
+const MIN_TO_MS_CONVERSION: number = 60000;
 /**
  * The list of enabled metric source integrations
  */
@@ -30,4 +31,30 @@ export const metricSourceIntegrations: () => KvMap<MetricSourceIntegration<Canar
  */
 export const metricSourceTypes: () => string[] = () => {
   return enabledMetricSources.map(metricSourceIntegration => metricSourceIntegration.type);
+};
+
+/**
+ * Calculates time labels for metric graphs based on start, length of canary, and number of data points
+ */
+export const defaultGraphDataMapper = (
+  lifetime: number,
+  dataPointCount: number,
+  startTimeMillis: number,
+  stepMillis: number
+): {
+  controlTimeLabels: number[];
+  experimentTimeLabels?: number[];
+} => {
+  let controlTimeLabels: number[] = [];
+  let scale: number;
+  if (lifetime > 0 && dataPointCount > 0) {
+    const lifetimeMillis: number = lifetime * MIN_TO_MS_CONVERSION;
+    scale = Math.round(lifetimeMillis / dataPointCount);
+  } else {
+    scale = stepMillis;
+  }
+  for (let i = 0, j = startTimeMillis; i < dataPointCount; i++, j += scale) {
+    controlTimeLabels.push(j);
+  }
+  return { controlTimeLabels };
 };
