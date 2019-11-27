155094808cdb9bad59cddaccc28fcd0d26df8a6e
Sean Lin <sean.lin@nike.com>
2019-11-26 16:41:59 -0800

    fix: Show error message when canary analysis status is TERMINAL (#46)

--- packages/client/src/components/shared/TerminalResult.tsx
+++ packages/client/src/components/shared/TerminalResult.tsx
@@ -7,19 +7,26 @@ interface TerminalResultProps {
 }
 
 @observer
-export default class IndividualMetricView extends React.Component<TerminalResultProps> {
+export default class TerminalResult extends React.Component<TerminalResultProps> {
   render(): React.ReactNode {
     const { stackTrace, error, errors } = this.props.exception.details;
     return (
       <div className="terminal-canary">
         <div className="main-error">{error}</div>
         <div className="stacktrace">
-          <div className="stacktrace-label">Error</div>
-          {errors.map((error: string) => (
-            <div>{error}</div>
-          ))}
-          <div className="stacktrace-label">Stacktrace</div>
-          <pre>{stackTrace}</pre>
+          {errors && (
+            <div>
+              <div className="stacktrace-label">Error</div>
+              errors.map((error: string) => (<div>{error}</div>
+              ))
+            </div>
+          )}
+          {stackTrace && (
+            <div>
+              <div className="stacktrace-label">Stacktrace</div>
+              <pre>{stackTrace}</pre>
+            </div>
+          )}
         </div>
       </div>
     );
