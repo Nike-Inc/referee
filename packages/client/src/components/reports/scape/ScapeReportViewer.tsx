8c32c9fa5ec01096b64b31c3ec06a870ea8220a2
Melana Hammel <Melana.Hammel@nike.com>
2020-03-13 13:50:51 -0700

    chore: updated error message wording

--- packages/client/src/components/reports/scape/ScapeReportViewer.tsx
+++ packages/client/src/components/reports/scape/ScapeReportViewer.tsx
@@ -91,9 +91,9 @@ export default class ScapeReportViewer extends ConnectedComponent<Props, Stores,
         0
       );
       if (totalGroupWeights !== 100) {
-        log.error(`Configuration Error: Group weights need to equal 100.`);
+        log.error(`Configuration Error: Group weights need to add up to 100.`);
         this.stores.errorStore.push({
-          heading: `Configuration Error: Group weights need to equal 100.`,
+          heading: `Configuration Error: Group weights need to add up to 100.`,
           content: (
             <div>
               Your group weights equal {totalGroupWeights}. This might affect your canary results. Please click "Go To Config" to set group weights to 100.
