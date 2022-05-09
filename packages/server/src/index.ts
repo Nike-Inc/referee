37391f74477b4156e9aa599335d9fdc70cca7cbb
D Jason Penney <djasonpenney@gmail.com>
2022-05-09 11:35:49 -0700

    feat/devintel-33: create a healthcheck endpoint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -51,6 +51,10 @@ app.post('/metrics', (req, res) => {
   res.status(201).end();
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).end();
+});
+
 app.use(
   '/kayenta',
   proxy(Optional.ofNullable(process.env.KAYENTA_BASE_URL).orElse('http://localhost:8090'), {
